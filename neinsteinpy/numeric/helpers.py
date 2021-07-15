c2330d844e80c73592fb05f5f40d653316d59067
Wenyin Wei <wenyin.wei.ww@gmail>
2021-07-15 21:08:11 +0800

    remove symbolic part of helpers.py

--- neinsteinpy/numeric/helpers.py
+++ neinsteinpy/numeric/helpers.py
@@ -8,16 +8,6 @@ from sympy import ImmutableDenseNDimArray, derive_by_array
 def raise_warning(WarningType, message):
     warnings.warn(message, WarningType)
 
-
-def _flatten_list(seq):
-    # flatten an arbitarily nested list
-    if not seq:
-        return []
-    if not isinstance(seq[0], list):
-        return [seq[0]] + _flatten_list(seq[1:])
-    return _flatten_list(seq[0]) + _flatten_list(seq[1:])
-
-
 class TransformationMatrix(ImmutableDenseNDimArray):
     """
     Class for defining transformation matrix for basis change of vectors and tensors.
