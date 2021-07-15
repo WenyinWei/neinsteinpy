f08622c091ce8a4bf031aa89587d9fc26ecec6ea
Wenyin Wei <wenyin.wei.ww@gmail>
2021-07-15 21:16:50 +0800

    numeric vector.py done

--- neinsteinpy/numeric/vector.py
+++ neinsteinpy/numeric/vector.py
@@ -1,26 +1,26 @@
-from einsteinpy.numeric.helpers import _change_name
-from einsteinpy.numeric.tensor import NBaseRelativityTensor, _change_config
+from neinsteinpy.numeric.helpers import _change_name
+from neinsteinpy.numeric.tensor import NBaseRelativityTensor, _change_config
 
 
 class NGenericVector(NBaseRelativityTensor):
     """
-    Class to represent a vector in arbitrary space-time symbolically
+    Class to represent a vector in arbitrary space-time numerically
 
     """
 
-    def __init__(self, arr, syms, config="u", parent_metric=None, name="GenericVector"):
+    def __init__(self, arr, vars, config="u", parent_metric=None, name="GenericVector"):
         """
         Constructor and Initializer
 
         Parameters
         ----------
-        arr : ~sympy.tensor.array.dense_ndim_array.ImmutableDenseNDimArray
-            Sympy Array containing Sympy Expressions
-        syms : tuple or list
+        arr : ~numpy.ndarray
+            Numpy Array of the vector field 
+        vars : tuple or list of 1-dim numpy.ndarray
             Tuple of crucial symbols denoting time-axis, 1st, 2nd, and 3rd axis (t,x1,x2,x3)
         config : str
             Configuration of contravariant and covariant indices in tensor. 'u' for upper and 'l' for lower indices. Defaults to 'u'.
-        parent_metric : ~einsteinpy.symbolic.metric.MetricTensor or None
+        parent_metric : ~neinsteinpy.numeric.metric.NMetricTensor or None
             Corresponding Metric for the Generic Vector.
             Defaults to None.
         name : str
@@ -35,7 +35,7 @@ class NGenericVector(NBaseRelativityTensor):
 
         """
         super(GenericVector, self).__init__(
-            arr=arr, syms=syms, config=config, parent_metric=parent_metric, name=name
+            arr=arr, vars=vars, config=config, parent_metric=parent_metric, name=name
         )
         if self.arr.rank() == 1:
             self._order = 1
@@ -52,14 +52,14 @@ class NGenericVector(NBaseRelativityTensor):
         ----------
         newconfig : str
             Specify the new configuration. Defaults to 'u'
-        metric : ~einsteinpy.symbolic.metric.MetricTensor or None
+        metric : ~neinsteinpy.numeric.metric.NMetricTensor or None
             Parent metric tensor for changing indices.
             Already assumes the value of the metric tensor from which it was initialized if passed with None.
             Defaults to None.
 
         Returns
         -------
-        ~einsteinpy.symbolic.vector.GenericVector
+        ~neinsteinpy.numeric.vector.NGenericVector
             New tensor with new configuration.
 
         Raises
@@ -73,36 +73,36 @@ class NGenericVector(NBaseRelativityTensor):
         if metric is None:
             raise Exception("Parent Metric not found, can't do configuration change")
         new_tensor = _change_config(self, metric, newconfig)
-        new_obj = GenericVector(
+        new_obj = NGenericVector(
             new_tensor,
-            self.syms,
+            self.vars,
             config=newconfig,
             parent_metric=metric,
             name=_change_name(self.name, context="__" + newconfig),
         )
         return new_obj
 
-    def lorentz_transform(self, transformation_matrix):
-        """
-        Performs a Lorentz transform on the vector.
+    # def lorentz_transform(self, transformation_matrix):
+    #     """
+    #     Performs a Lorentz transform on the vector.
 
-        Parameters
-        ----------
-            transformation_matrix : ~sympy.tensor.array.dense_ndim_array.ImmutableDenseNDimArray or list
-                Sympy Array or multi-dimensional list containing Sympy Expressions
+    #     Parameters
+    #     ----------
+    #         transformation_matrix : ~sympy.tensor.array.dense_ndim_array.ImmutableDenseNDimArray or list
+    #             Sympy Array or multi-dimensional list containing Sympy Expressions
 
-        Returns
-        -------
-            ~einsteinpy.symbolic.vector.GenericVector
-                lorentz transformed vector
+    #     Returns
+    #     -------
+    #         ~einsteinpy.symbolic.vector.GenericVector
+    #             lorentz transformed vector
 
-        """
+    #     """
 
-        t = super(GenericVector, self).lorentz_transform(transformation_matrix)
-        return GenericVector(
-            t.tensor(),
-            syms=self.syms,
-            config=self.config,
-            parent_metric=None,
-            name=_change_name(self.name, context="__lt"),
-        )
+    #     t = super(GenericVector, self).lorentz_transform(transformation_matrix)
+    #     return GenericVector(
+    #         t.tensor(),
+    #         vars=self.vars,
+    #         config=self.config,
+    #         parent_metric=None,
+    #         name=_change_name(self.name, context="__lt"),
+    #     )
