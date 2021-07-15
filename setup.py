7f30b3b4142e55f4a0a623a8212c201ac9c50a3a
Wenyin Wei <wenyin.wei.ww@gmail>
2021-07-15 21:32:33 +0800

    update README and setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setuptools.setup(
     version="0.0.1",
     author="Wenyin Wei",
     author_email="wenyin.wei@ipp.ac.cn",
-    description="The neinsteinpy package is a numeric version of einsteinpy, which is well-known for its well ability on tensor symbol operation.",
+    description="The neinsteinpy package is a numeric version of einsteinpy, which is well-known due to its good ability on tensor symbol operation.",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/WenyinWei/neinsteinpy",
